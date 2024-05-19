05806253495038b9a76b2e058a70046e71a767cc
Lambda <l@ambda.com>
2024-05-19 21:27:28 +0200

    morestuff

--- bindings.js
+++ bindings.js
@@ -4,7 +4,7 @@ try {
 } catch(_) {
   // Use a remote version if there is no local version
   const os = require('os');
-  module.exports = require(`@roamhq/beamcoder-${os.platform()}-${os.arch()}`);
+  module.exports = require(`@lambdacerro/beamcoder-${os.platform()}-${os.arch()}`);
 }
 
 // TODO(jack): more informative error messages on failure
--- prebuilds/win32-x64/README.md
+++
@@ -1,4 +0,0 @@
-# beamcoder
-
-This is the Windows 64-bit binary build for beamcoder.
-See https://github.com/WonderInventions/beamcoder for details.
